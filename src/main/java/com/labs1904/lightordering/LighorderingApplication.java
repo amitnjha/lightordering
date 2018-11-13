a120745b93728fa027742bb03feefbfc6fad3026
Amit Jha <amitnjha@gmail.com>
2018-11-13 01:40:41 -0600

    initial commit

---
+++ src/main/java/com/labs1904/lightordering/LighorderingApplication.java
@@ -0,0 +1,13 @@
+package com.labs1904.lightordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LighorderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LighorderingApplication.class, args);
+		System.out.println("Working on it");
+	}
+}
