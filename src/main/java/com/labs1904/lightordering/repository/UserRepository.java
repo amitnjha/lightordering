0a69bd680d577f3de33e9d91056cdec05b321bc2
Amit Jha <amitnjha@gmail.com>
2018-11-20 21:01:26 -0600

    with error

--- src/main/java/com/labs1904/lightordering/repository/UserRepository.java
+++ src/main/java/com/labs1904/lightordering/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.labs1904.lightordering.repository;
 
+import java.util.List;
+
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import com.labs1904.lightordering.entity.UserEntity;
 
-public interface UserRepository extends CrudRepository<UserEntity, Integer>{
+public interface UserRepository extends CrudRepository<UserEntity, Integer>, QuerydslPredicateExecutor<UserEntity> {
 
-}
+	List<UserEntity> findByEmail(String email);
+};
